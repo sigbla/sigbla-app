c30c123e78153738a29f2d16aaa585b5f060465b
Christian Felde <cfelde@cfelde.com>
2023-05-29 14:48:14 +0100

    table.js refactoring, making most fields and methods private

--- app/src/main/resources/table/table.js
+++ app/src/main/resources/table/table.js
@@ -1,148 +1,148 @@
 class Sigbla {
-    targetParent
-    haveInit
+    #targetParent
+    #haveInit
 
-    target
-    corner
-    end
-    overlay
-    ovl
-    ohl
+    #target
+    #corner
+    #end
+    #overlay
+    #ovl
+    #ohl
 
-    socket
+    #socket
 
-    lastTile = [-1, -1, -1, -1]
+    #lastTile = [-1, -1, -1, -1]
 
-    pendingContent = null
-    pendingUpdate = false
-    pendingScrolls = []
-    pendingResize = []
+    #pendingContent = null
+    #pendingUpdate = false
+    #pendingScrolls = []
+    #pendingResize = []
     // TODO Change this from being events to being function calls, that way we can change the dom before mounting etc
-    pendingEvents = []
+    #pendingEvents = []
 
-    resizeTarget
-    resizeStartX
-    resizeStartY
-    enableVerticalOverlay = false
-    enableHorizontalOverlay = false
+    #resizeTarget
+    #resizeStartX
+    #resizeStartY
+    #enableVerticalOverlay = false
+    #enableHorizontalOverlay = false
 
-    swapBuffer = false
+    #swapBuffer = false
 
     constructor(targetParent) {
-        this.targetParent = targetParent
-        this.targetParent.innerHTML = ""
+        this.#targetParent = targetParent
+        this.#targetParent.innerHTML = ""
 
-        this.target = document.createElement("div")
+        this.#target = document.createElement("div")
 
         // debug this.targetParent.addEventListener("test-event", (e) => console.log(e))
     }
 
-    rbMousedown = (e) => {
+    #rbMousedown = (e) => {
         if (e.buttons === 1) {
-            this.resizeTarget = e.target.parentElement.parentElement.id
+            this.#resizeTarget = e.target.parentElement.parentElement.id
 
-            this.enableVerticalOverlay = true
-            this.enableHorizontalOverlay = false
+            this.#enableVerticalOverlay = true
+            this.#enableHorizontalOverlay = false
 
-            this.resizeStartX = e.clientX
-            this.resizeStartY = e.clientY
+            this.#resizeStartX = e.clientX
+            this.#resizeStartY = e.clientY
 
-            this.overlay.style.display = "block"
-            this.ohl.style.display = "none"
-            this.ovl.style.display = "block"
-            this.ovl.style.left = e.clientX + "px"
+            this.#overlay.style.display = "block"
+            this.#ohl.style.display = "none"
+            this.#ovl.style.display = "block"
+            this.#ovl.style.left = e.clientX + "px"
         }
     }
 
-    bbMousedown = (e) => {
+    #bbMousedown = (e) => {
         if (e.buttons === 1) {
-            this.resizeTarget = e.target.parentElement.parentElement.id
+            this.#resizeTarget = e.target.parentElement.parentElement.id
 
-            this.enableVerticalOverlay = false
-            this.enableHorizontalOverlay = true
+            this.#enableVerticalOverlay = false
+            this.#enableHorizontalOverlay = true
 
-            this.resizeStartX = e.clientX
-            this.resizeStartY = e.clientY
+            this.#resizeStartX = e.clientX
+            this.#resizeStartY = e.clientY
 
-            this.overlay.style.display = "block"
-            this.ovl.style.display = "none"
-            this.ohl.style.display = "block"
-            this.ohl.style.top = e.clientY + "px"
+            this.#overlay.style.display = "block"
+            this.#ovl.style.display = "none"
+            this.#ohl.style.display = "block"
+            this.#ohl.style.top = e.clientY + "px"
         }
     }
 
-    submitResize = (target, sizeChangeX, sizeChangeY) => {
+    #submitResize = (target, sizeChangeX, sizeChangeY) => {
         const resizeEvent = { type: 8, target: target, sizeChangeX: sizeChangeX, sizeChangeY: sizeChangeY}
 
-        if (this.pendingUpdate) {
-            this.pendingResize.push(resizeEvent)
+        if (this.#pendingUpdate) {
+            this.#pendingResize.push(resizeEvent)
         } else {
-            this.pendingUpdate = true
-            this.socket.send(JSON.stringify(resizeEvent))
+            this.#pendingUpdate = true
+            this.#socket.send(JSON.stringify(resizeEvent))
         }
     }
 
-    overlayMouseup = (e) => {
-        if (this.enableVerticalOverlay) {
-            const resizeX = e.clientX - this.resizeStartX
+    #overlayMouseup = (e) => {
+        if (this.#enableVerticalOverlay) {
+            const resizeX = e.clientX - this.#resizeStartX
             if (resizeX !== 0) {
-                this.submitResize(this.resizeTarget, resizeX, 0)
+                this.#submitResize(this.#resizeTarget, resizeX, 0)
             } else {
-                this.overlay.style.display = "none"
+                this.#overlay.style.display = "none"
             }
-        } else if (this.enableHorizontalOverlay) {
-            const resizeY = e.clientY - this.resizeStartY
+        } else if (this.#enableHorizontalOverlay) {
+            const resizeY = e.clientY - this.#resizeStartY
             if (resizeY !== 0) {
-                this.submitResize(this.resizeTarget, 0, resizeY)
+                this.#submitResize(this.#resizeTarget, 0, resizeY)
             } else {
-                this.overlay.style.display = "none"
+                this.#overlay.style.display = "none"
             }
         }
 
-        this.ovl.style.display = "none"
-        this.ohl.style.display = "none"
+        this.#ovl.style.display = "none"
+        this.#ohl.style.display = "none"
 
-        this.enableVerticalOverlay = false
-        this.enableHorizontalOverlay = false
+        this.#enableVerticalOverlay = false
+        this.#enableHorizontalOverlay = false
     }
 
-    overlayMousemove = (e) => {
-        if (this.enableVerticalOverlay) this.ovl.style.left = e.clientX + "px"
-        if (this.enableHorizontalOverlay) this.ohl.style.top = e.clientY + "px"
+    #overlayMousemove = (e) => {
+        if (this.#enableVerticalOverlay) this.#ovl.style.left = e.clientX + "px"
+        if (this.#enableHorizontalOverlay) this.#ohl.style.top = e.clientY + "px"
     }
 
-    renderInit = () => {
-        this.target.innerHTML = ""
+    #renderInit = () => {
+        this.#target.innerHTML = ""
 
-        this.overlay = document.createElement("div")
-        this.overlay.className = "overlay"
-        this.overlay.onmouseup = this.overlayMouseup
-        this.overlay.onmousemove = this.overlayMousemove
+        this.#overlay = document.createElement("div")
+        this.#overlay.className = "overlay"
+        this.#overlay.onmouseup = this.#overlayMouseup
+        this.#overlay.onmousemove = this.#overlayMousemove
 
-        this.ovl = document.createElement("div")
-        this.ovl.className = "ovl"
+        this.#ovl = document.createElement("div")
+        this.#ovl.className = "ovl"
 
-        this.ohl = document.createElement("div")
-        this.ohl.className = "ohl"
+        this.#ohl = document.createElement("div")
+        this.#ohl.className = "ohl"
 
-        this.overlay.appendChild(this.ovl)
-        this.overlay.appendChild(this.ohl)
+        this.#overlay.appendChild(this.#ovl)
+        this.#overlay.appendChild(this.#ohl)
 
-        this.targetParent.appendChild(this.overlay)
+        this.#targetParent.appendChild(this.#overlay)
     }
 
-    stateInit = () => {
-        this.lastTile = [-1, -1, -1, -1]
-        this.pendingContent = null
-        this.pendingUpdate = false
-        this.pendingScrolls = []
-        this.pendingResize = []
-        this.enableVerticalOverlay = false
-        this.enableHorizontalOverlay = false
-        this.swapBuffer = false
+    #stateInit = () => {
+        this.#lastTile = [-1, -1, -1, -1]
+        this.#pendingContent = null
+        this.#pendingUpdate = false
+        this.#pendingScrolls = []
+        this.#pendingResize = []
+        this.#enableVerticalOverlay = false
+        this.#enableHorizontalOverlay = false
+        this.#swapBuffer = false
     }
 
-    scroll = async () => {
+    #scroll = async () => {
         const inc = 100
 
         const xoffset = window.pageXOffset
@@ -154,27 +154,27 @@ class Sigbla {
         const xtile = xoffset - (xoffset % inc)
         const ytile = yoffset - (yoffset % inc)
 
-        if (this.lastTile[0] === xtile && this.lastTile[1] === ytile && this.lastTile[2] === height && this.lastTile[3] === width) return
+        if (this.#lastTile[0] === xtile && this.#lastTile[1] === ytile && this.#lastTile[2] === height && this.#lastTile[3] === width) return
 
-        this.lastTile = [xtile, ytile, height, width]
+        this.#lastTile = [xtile, ytile, height, width]
 
         // type 1 is scroll event
         const scrollEvent = { type: 1, x: xtile, y: ytile, h: height, w: width}
 
-        if (this.pendingUpdate) {
-            this.pendingScrolls.push(scrollEvent)
+        if (this.#pendingUpdate) {
+            this.#pendingScrolls.push(scrollEvent)
         } else {
-            this.pendingUpdate = true
-            this.socket.send(JSON.stringify(scrollEvent))
+            this.#pendingUpdate = true
+            this.#socket.send(JSON.stringify(scrollEvent))
         }
     }
 
-    socketOpen = async (_) => {
-        window.addEventListener("scroll", this.scroll)
-        window.addEventListener("resize", this.scroll)
+    #socketOpen = async (_) => {
+        window.addEventListener("scroll", this.#scroll)
+        window.addEventListener("resize", this.#scroll)
     }
 
-    dynamicallyLoadStyle = (url) => {
+    #dynamicallyLoadStyle = (url) => {
         let pathname = location.pathname.endsWith("/") ? location.pathname : location.pathname + "/"
         let fullURL = new URL(pathname + "resources/" + url, window.location.href).href
 
@@ -192,7 +192,7 @@ class Sigbla {
         document.head.appendChild(link)
     }
 
-    dynamicallyLoadScript = (url) => {
+    #dynamicallyLoadScript = (url) => {
         let pathname = location.pathname.endsWith("/") ? location.pathname : location.pathname + "/"
         let fullURL = new URL(pathname + "resources/" + url, window.location.href).href
 
@@ -208,21 +208,21 @@ class Sigbla {
         document.head.appendChild(script)
     }
 
-    handleMessage = async (message) => {
+    #handleMessage = async (message) => {
         switch (message.type) {
             case 0: { // clear
                 // TODO Probably should add some type of event for this as well..
 
-                this.target = document.createElement("div")
-                this.corner = null
-                this.end = null
+                this.#target = document.createElement("div")
+                this.#corner = null
+                this.#end = null
 
-                this.lastTile = [-1, -1, -1, -1]
-                this.swapBuffer = true
-                this.pendingScrolls.length = 0
-                this.pendingResize.length = 0
-                this.pendingUpdate = false
-                this.pendingContent = null
+                this.#lastTile = [-1, -1, -1, -1]
+                this.#swapBuffer = true
+                this.#pendingScrolls.length = 0
+                this.#pendingResize.length = 0
+                this.#pendingUpdate = false
+                this.#pendingContent = null
 
                 break
             }
@@ -255,11 +255,11 @@ class Sigbla {
 
                     const bb = document.createElement("div")
                     bb.className = "bb"
-                    bb.onmousedown = this.bbMousedown
+                    bb.onmousedown = this.#bbMousedown
 
                     const rb = document.createElement("div")
                     rb.className = "rb"
-                    rb.onmousedown = this.rbMousedown
+                    rb.onmousedown = this.#rbMousedown
 
                     child.appendChild(bb)
                     child.appendChild(rb)
@@ -293,23 +293,23 @@ class Sigbla {
                     })))
                     old.remove()
 
-                    if (message.content !== null) this.target.appendChild(div)
+                    if (message.content !== null) this.#target.appendChild(div)
                 } else if (message.content !== null) {
-                    if (this.pendingContent === null) {
-                        this.pendingContent = document.createDocumentFragment()
+                    if (this.#pendingContent === null) {
+                        this.#pendingContent = document.createDocumentFragment()
                     }
 
-                    this.pendingContent.appendChild(div)
+                    this.#pendingContent.appendChild(div)
                 }
 
-                (message.topics || []).forEach((e) => this.pendingEvents.push([div, e]))
+                (message.topics || []).forEach((e) => this.#pendingEvents.push([div, e]))
 
                 break
             }
             case 3: { // add commit
-                if (this.pendingContent === null) break
-                this.target.appendChild(this.pendingContent)
-                this.pendingContent = null
+                if (this.#pendingContent === null) break
+                this.#target.appendChild(this.#pendingContent)
+                this.#pendingContent = null
                 break
             }
             case 4: { // remove content
@@ -326,83 +326,83 @@ class Sigbla {
                 break
             }
             case 5: { // update end
-                if (this.swapBuffer) {
-                    this.swapBuffer = false
+                if (this.#swapBuffer) {
+                    this.#swapBuffer = false
 
                     document.getElementById("target").remove()
 
-                    this.target.id = "target"
-                    this.targetParent.appendChild(this.target)
+                    this.#target.id = "target"
+                    this.#targetParent.appendChild(this.#target)
 
-                    this.overlay.style.display = "none"
+                    this.#overlay.style.display = "none"
                 }
 
-                this.pendingEvents.forEach((divEvent) => divEvent[0].dispatchEvent(new CustomEvent(divEvent[1], {
+                this.#pendingEvents.forEach((divEvent) => divEvent[0].dispatchEvent(new CustomEvent(divEvent[1], {
                     bubbles: true,
                     detail: {
                         action: "show"
                     }
                 })))
 
-                this.pendingEvents = []
+                this.#pendingEvents = []
 
                 break
             }
             case 6: { // package end
-                this.socket.send(JSON.stringify({ type: 6, id: message.id }))
+                this.#socket.send(JSON.stringify({ type: 6, id: message.id }))
 
                 let havePendingResize = false
                 let havePendingScroll = false
 
-                if (this.pendingResize.length > 0) {
-                    for (let i = 0; i < this.pendingResize.length; i++) {
-                        this.socket.send(JSON.stringify(this.pendingResize[i]))
+                if (this.#pendingResize.length > 0) {
+                    for (let i = 0; i < this.#pendingResize.length; i++) {
+                        this.#socket.send(JSON.stringify(this.#pendingResize[i]))
                     }
-                    this.pendingResize.length = 0
+                    this.#pendingResize.length = 0
                     havePendingResize = true
                 }
 
-                if (this.pendingScrolls.length > 0) {
-                    let pendingScroll = this.pendingScrolls.pop()
-                    this.pendingScrolls.length = 0
-                    this.socket.send(JSON.stringify(pendingScroll))
+                if (this.#pendingScrolls.length > 0) {
+                    let pendingScroll = this.#pendingScrolls.pop()
+                    this.#pendingScrolls.length = 0
+                    this.#socket.send(JSON.stringify(pendingScroll))
                     havePendingScroll = true
                 }
 
-                this.pendingUpdate = havePendingResize || havePendingScroll
+                this.#pendingUpdate = havePendingResize || havePendingScroll
 
                 break
             }
             case 7: { // dims
-                const corner = this.corner || (() => {
+                const corner = this.#corner || (() => {
                     const newCorner = document.createElement("div")
                     newCorner.className = "tc"
-                    this.target.appendChild(newCorner)
-                    this.corner = newCorner
+                    this.#target.appendChild(newCorner)
+                    this.#corner = newCorner
                     return newCorner
                 })()
 
                 corner.style.height = (message.cornerY-1) + "px"
                 corner.style.width = (message.cornerX-1) + "px"
 
-                this.target.style.height = message.maxY + "px"
-                this.target.style.width = message.maxX + "px"
+                this.#target.style.height = message.maxY + "px"
+                this.#target.style.width = message.maxX + "px"
 
-                const end = this.end || (() => {
+                const end = this.#end || (() => {
                     const newEnd = document.createElement("div")
                     newEnd.style.position = "absolute"
                     newEnd.style.width = "1px"
                     newEnd.style.height = "1px"
                     newEnd.style.backgroundColor = "black"
-                    this.target.appendChild(newEnd)
-                    this.end = newEnd
+                    this.#target.appendChild(newEnd)
+                    this.#end = newEnd
                     return newEnd
                 })()
 
                 end.style.top = message.maxY + "px"
                 end.style.left = message.maxX + "px"
 
-                await this.scroll()
+                await this.#scroll()
 
                 break
             }
@@ -413,42 +413,44 @@ class Sigbla {
             }
             case 9: {
                 // Load CSS
-                (message.urls || []).forEach(this.dynamicallyLoadStyle)
+                (message.urls || []).forEach(this.#dynamicallyLoadStyle)
 
                 break
             }
             case 10: {
                 // Load JS
-                (message.urls || []).forEach(this.dynamicallyLoadScript)
+                (message.urls || []).forEach(this.#dynamicallyLoadScript)
 
                 break
             }
         }
     }
 
-    socketMessage = async (e) => {
+    #socketMessage = async (e) => {
         let messages = JSON.parse(e.data)
-        if (!Array.isArray(messages)) await this.handleMessage(messages)
-        else messages.forEach(await this.handleMessage)
+        if (!Array.isArray(messages)) await this.#handleMessage(messages)
+        else messages.forEach(await this.#handleMessage)
     }
 
     init = () => {
-        if (this.haveInit) return
-        this.haveInit = true
+        if (this.#haveInit) return false
+        this.#haveInit = true
 
-        this.renderInit()
-        this.stateInit()
+        this.#renderInit()
+        this.#stateInit()
 
-        this.target.id = "target"
-        this.targetParent.appendChild(this.target)
+        this.#target.id = "target"
+        this.#targetParent.appendChild(this.#target)
 
         let pathname = location.pathname.endsWith("/") ? location.pathname : location.pathname + "/"
         let url = new URL(pathname + "socket", window.location.href)
         url.protocol = url.protocol.replace('https', 'wss')
         url.protocol = url.protocol.replace('http', 'ws')
 
-        this.socket = new WebSocket(url.href)
-        this.socket.addEventListener("open", this.socketOpen)
-        this.socket.addEventListener("message", this.socketMessage)
+        this.#socket = new WebSocket(url.href)
+        this.#socket.addEventListener("open", this.#socketOpen)
+        this.#socket.addEventListener("message", this.#socketMessage)
+
+        return true
     }
 }
