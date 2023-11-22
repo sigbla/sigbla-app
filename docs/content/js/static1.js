d7ec35e7d8cdf24358797298e2f588b4128dfa27
Christian Felde <cfelde@cfelde.com>
2023-11-22 17:46:26 +0000

    Documentation updates

---
+++ docs/content/js/static1.js
@@ -0,0 +1,4 @@
+const script = document.createElement("script");
+script.setAttribute("data-domain", "sigbla.app");
+script.setAttribute("src", "https://static1.sigbla.app/js/script.js");
+document.head.appendChild(script);
