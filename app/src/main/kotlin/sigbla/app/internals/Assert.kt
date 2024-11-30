3b1099ae6a1053116c2b3d535d3113e775c7fa2b
Christian Felde <cfelde@cfelde.com>
2024-11-30 21:18:34 +0000

    New release: 1.24.6 - 2024-11-30 - Winter Pine

--- app/src/main/kotlin/sigbla/app/internals/Assert.kt
+++ app/src/main/kotlin/sigbla/app/internals/Assert.kt
@@ -1,3 +1,5 @@
+/* Copyright 2019-2024, Christian Felde.
+ * See LICENSE file for licensing details. */
 package sigbla.app.internals
 
 internal val enableAssert: Boolean = System.getProperty("sigbla.assert") == "true"
